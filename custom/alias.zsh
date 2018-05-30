90b65538ded14c31e0928fadfa84a7ab7235c544
Kaidong Hu <dongforregister@gmail.com>
2018-05-30 12:09:43 -0400

    .

--- custom/alias.zsh
+++ custom/alias.zsh
@@ -4,6 +4,11 @@ alias l='ls'
 alias sa='source activate'
 alias sd='source deactivate'
 
-function gi() { curl -L -s https://www.gitignore.io/api/$@;}
+supressed(){
+  screen -dmS $1 $@
+}
+gi() { 
+  curl -L -s https://www.gitignore.io/api/$@
+}
 hash thefuck 2>/dev/null && { eval $(thefuck --alias f) }
 
