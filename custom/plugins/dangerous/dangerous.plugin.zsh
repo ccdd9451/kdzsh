203ccee87b15ff298c478c8566ea88e77f44ed15
Kaidong Hu <dongforregister@gmail.com>
2018-08-28 23:56:11 -0400

    .

--- custom/plugins/dangerous/dangerous.plugin.zsh
+++ custom/plugins/dangerous/dangerous.plugin.zsh
@@ -1,6 +1,15 @@
 alias x='chmod a+x $_; $_'
 alias open='open_command'
-
+function clearls {
+  if [[ `pwd` -ef $HOME ]]; then
+    ls -IDesktop -IDocuments -IDownloads -IMusic -IPictures\
+       -IPublic -ITemplates -IVideos -Isnap --color=tty;
+  else
+    ls --color=tty;
+  fi
+}
+alias ls=clearls
+alias la="\ls --color=tty -lAh"
 export PATH=$PATH:'.'
 
 if [ `uname` = 'Linux' ]
